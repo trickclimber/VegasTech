3128ea277462b62ac1d0c9528c9e18fe61882b9f
Patrick Olson <trick_climber@yahoo.com>
2011-09-29 00:05:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vegastech::Application.config.secret_token = '5418539a32a15254f11844c0e4cdd0917c43035a7b367c7325fb0501f4cc34e9c9fdb9cae6a0e7012af4bfb7792fa94add30aefcd23838268969b0893d5d0c15'
